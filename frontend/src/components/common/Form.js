8fa19ffce62ed4e283955bc58d414be241b27ab0
Francis Causing <franzcsng23@gmail.com>
2025-09-21 20:56:31 +0800

    Complete: refractor form component to accept data and route props for submission

--- frontend/src/components/common/Form.js
+++ frontend/src/components/common/Form.js
@@ -1,25 +1,48 @@
 import './Form.scss'
+import {useState} from 'react'
+import axios from 'axios'
 
 const Form = (props) => {
 
+    const [data, setData] = useState(props.data)
     const fields = props.fields
-    const generateFields = (fields) => {
-        // { name: "", type: "", placeholder: ""}
+    const route = props.route
 
-        return fields.map((field, index) => 
-            (
-                <>
-                    <label key={index} for={field.name}>{field.name}</label>
-                    <input type={field.type} id={field.name} name={field.name} placeholder={field.placeholder}></input>
-                </>
-            )
+    const generateFields = (fields, data, setData) => {
+        return fields.map((field, index) => {
+            const fieldKey = field.value
+                return (
+                    <>
+                    
+                        <label key={index} htmlFor={field.name}>{field.name}</label>
+                        <input 
+                            onChange={(e) => setData({...data, [fieldKey]: e.target.value})}
+                            type={field.type} 
+                            id={field.name} 
+                            name={field.name} 
+                            placeholder={field.placeholder}>
+
+                        </input>
+                    </>
+                )
+            }
         )
     }
 
-    return (
-        <form className='form-main'>
+    const handleSubmit = (e, route) => {
+        e.preventDefault()
+        
+        axios.post(`http://localhost:5000${route}`, data)
+        .then((res) => {
+            console.log(`Successfully added the following: ${res}`)
+        })
+        .catch((err) => console.log(err))
+    }
 
-            {fields && generateFields(fields)}
+    return (
+        <form onSubmit={(e)=>handleSubmit(e, route)}className='form-main'>
+
+            {fields && generateFields(fields, data, setData)}
             <button type='submit'>ADD</button>
 
         </form>
