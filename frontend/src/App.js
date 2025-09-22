f07be0e6ab54a76b2e62f997499d259a260b06a2
Francis Causing <franzcsng23@gmail.com>
2025-09-23 00:50:58 +0800

    Add: add Update page component into routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import {Route, createBrowserRouter, RouterProvider, createRoutesFromElements} from 'react-router-dom'
-
 import Layout from './pages/layout/Layout.js'
 import Home from './pages/home/Home.js'
+import Update from './pages/update/Update.js'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path ='/' element={<Layout/>}>
       <Route index element={<Home/>}/>
+      <Route path='/item/:id' element={<Update/>}/>
     </Route>
   )
 )
