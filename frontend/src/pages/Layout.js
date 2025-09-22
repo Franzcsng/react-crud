f6ce85ea87858a1c82a9b777272b0699dbf9c280
Francis Causing <franzcsng23@gmail.com>
2025-09-23 00:50:14 +0800

    Add: add layout component for navigation outlet

---
+++ frontend/src/pages/Layout.js
@@ -0,0 +1,12 @@
+import './Layout.scss'
+import { Outlet } from 'react-router'
+
+const Layout = () => {
+    return(
+        <div>
+            <Outlet/>
+        </div>
+    )
+}
+
+export default Layout
