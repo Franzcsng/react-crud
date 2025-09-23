4c888eb2ada43e28e0e596b8952fe5f9f982ba99
Francis Causing <franzcsng23@gmail.com>
2025-09-23 16:48:01 +0800

    Complete: finish update functionality in Update page

--- frontend/src/pages/update/Update.js
+++ frontend/src/pages/update/Update.js
@@ -5,37 +5,50 @@ import {useState , useEffect} from 'react'
 import axios from 'axios'
 
 const Update = () => {
+
     let params = useParams();
     const id = params.id;
-
-    const [item, setItem] = useState({})
+    const [data, setData] =  useState({})
 
     useEffect(() => {
         axios.get(`/item/${id}`)
         .then((res) => {
-            console.log(res)
-            setItem(res.data[0])
+            console.log(res.data[0])
+            setData(res.data[0])
         })
+        .catch((err) => console.log(err))
+        
     }, [])
+
+
+    const handleSubmit = (e, route) => {
+        e.preventDefault()
+
+        axios.put(`/item/${id}`, data)
+        .then((res) => {
+            console.log('Item updated succesfully: ' +  res)
+        })
+        .catch((err) => {
+            console.log('Error updating item: ' + err)
+        }) 
+    }
+
 
     return (
         <div className='update-page'>
             <div className='update-wrapper'>
             
-                <h1>{`ITEM: ${item.item_name}, ID: ${item.id}`}</h1>
+                <h1>{`ITEM: ${data.item_name}, ID: ${data.id}`}</h1>
                 <Form
                     fields={[
-                        {name: "Item", type: "text", placeholder: "Enter item name", value: 'item_name', itemValue: item.item_name},
-                        {name: "Price", type: "text", placeholder: "Enter price", value: 'item_price', itemValue: item.item_price},
-                        {name: "Quantity", type: "text", placeholder: "Enter quantity", value: 'item_quantity', itemValue: item.item_quantity},
+                        {name: "Item", type: "text", placeholder: "Enter item name", value: 'item_name' },
+                        {name: "Price", type: "text", placeholder: "Enter price", value: 'item_price' },
+                        {name: "Quantity", type: "text", placeholder: "Enter quantity", value: 'item_quantity'},
                     ]}
-                    data={{
-                        item_name: '',
-                        item_price: '',
-                        item_quantity: '',
-                    }}
+                    data={data}
+                    setData={setData}
 
-                    route={`/item/${id}`}
+                    submit={handleSubmit}
                     button='UPDATE'
                 />
             </div>
