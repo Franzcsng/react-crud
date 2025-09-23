1ecc7b9572d97196e60813f6c22f60a594534e1c
Francis Causing <franzcsng23@gmail.com>
2025-09-23 16:48:51 +0800

    Refractor: move data and handlesubmit into add page, to be passed into form component

--- frontend/src/pages/home/add/Add.js
+++ frontend/src/pages/home/add/Add.js
@@ -1,11 +1,32 @@
 import './Add.scss'
 import Form from '../../../components/common/Form.js'
 import {useState } from 'react'
+import axios from 'axios'
 
 const Add = (props) => {
     let display;
     props.display ? display = props.display : display ='none'
     const setDisplay = props.set 
+    const setUpdate = props.checkUpdate
+    const [data, setData] = useState({
+                        item_name: '',
+                        item_price: '',
+                        item_quantity: '',
+                    })
+    
+
+    const handleSubmit = (e, route) => {
+        e.preventDefault()
+        
+        axios.post(`http://localhost:5000/items`, data)
+        .then((res) => {
+            setUpdate(true)
+            console.log(`Successfully added the following: ${res}`)
+        })
+        .catch((err) => console.log(err))
+    }
+
+
     return(
         <div 
         className='add-modal' style={{display: `${display}`}} 
@@ -23,14 +44,9 @@ const Add = (props) => {
                         {name: "Price", type: "text", placeholder: "Enter price", value: 'item_price'},
                         {name: "Quantity", type: "text", placeholder: "Enter quantity", value: 'item_quantity'},
                     ]}
-                    data={{
-                        item_name: '',
-                        item_price: '',
-                        item_quantity: '',
-                    }}
-
-                    route='/items'
-
+                    data={data}
+                    setData={setData}
+                    submit={handleSubmit}
                     checkUpdate={props.checkUpdate}
                 />
             </div>
