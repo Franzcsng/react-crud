a1ec8df95d0e3f62b76ddefaead5d0ae6fef2d6f
Francis Causing <franzcsng23@gmail.com>
2025-09-24 22:56:13 +0800

    Add: add footer and header components

---
+++ frontend/src/components/common/header/Header.js
@@ -0,0 +1,12 @@
+import './Header.scss'
+
+
+const Header = () => {
+    return (
+        <div className='header'>   
+            <h1> STOCKAGE </h1>
+        </div>
+    )
+}
+
+export default Header
