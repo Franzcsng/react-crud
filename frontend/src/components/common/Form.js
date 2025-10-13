24c52bdc1db4840dea4e87449efe45b0a9b404ce
Francis Causing <franzcsng23@gmail.com>
2025-10-13 21:57:14 +0800

    Refractor: extract Input field into reusable component and reduced properties required

--- frontend/src/components/common/Form.js
+++ frontend/src/components/common/Form.js
@@ -1,39 +1,37 @@
 import './Form.scss'
+import Input from './input/Input.js'
 import {useState} from 'react'
 
 
 const Form = (props) => {
-
-    const data = props.data
-    const setData = props.setData
-    const fields = props.fields
-    const route = props.route
-    const handleSubmit = props.submit;
     
-
+    const data = props.data //data state variable to hold values of inputs before submit
+    const setData = props.setData //set function of data state hook to set values to state variable
+    const fields = props.fields //object array containing field information (type, label, name, etc.) to be passed into input component
+    const handleSubmit = props.submit; //handleSubmit function declared from parent component
+    
+    // Generate number of input fields depending on contents of object array "fields" props
     const generateFields = (fields, data, setData) => {
         return fields.map((field, index) => {
             const fieldKey = field.value
                 return (
                     <div  key={index}>
-                    
-                        <label htmlFor={field.name}>{field.name}</label> <br></br>
-                        <input 
-                            onChange={(e) => setData({...data, [fieldKey]: e.target.value})}
-                            type={field.type} 
-                            id={field.name} 
-                            name={field.name} 
+                        <Input
+                            label={field.label}
+                            name={field.name}
+                            type={field.type}
                             placeholder={field.placeholder}
-                            value={data[fieldKey] || ''}>
-                        </input>
+                            setData={(e) => setData({...data, [fieldKey]: e.target.value})}
+                        />
                     </div>
                 )
             }
         )
     }
 
+
     return (
-        <form onSubmit={(e)=>handleSubmit(e, route)}className='form-main'>
+        <form onSubmit={(e)=>handleSubmit(e)}className='form-main'>
 
             {fields && generateFields(fields, data, setData)}
             <button type='submit'>{props.button}</button>
