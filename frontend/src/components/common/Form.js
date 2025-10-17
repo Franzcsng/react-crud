f4cdce7cb98b83e16ccec2ddb99f76b4d7e268d3
Francis Causing <franzcsng23@gmail.com>
2025-10-17 22:33:35 +0800

    Edit: Add react-hook-form implementation in input and forms (in progress)

--- frontend/src/components/common/Form.js
+++ frontend/src/components/common/Form.js
@@ -1,7 +1,7 @@
 import './Form.scss'
 import Input from './input/Input.js'
 import {useState} from 'react'
-
+import { useForm, FormProvider, useFormContext} from 'react-hook-form'
 
 const Form = (props) => {
     
@@ -10,6 +10,14 @@ const Form = (props) => {
     const fields = props.fields //object array containing field information (type, label, name, etc.) to be passed into input component
     const handleSubmit = props.submit; //handleSubmit function declared from parent component
     
+    const methods = useForm()
+    
+
+    const onSubmit = methods.handleSubmit(data => {
+        console.log(data)
+    })
+
+
     // Generate number of input fields depending on contents of object array "fields" props
     const generateFields = (fields, data, setData) => {
         return fields.map((field, index) => {
@@ -31,12 +39,17 @@ const Form = (props) => {
 
 
     return (
-        <form onSubmit={(e)=>handleSubmit(e)}className='form-main'>
+        <FormProvider {...methods}>
+        {/* <form onSubmit={(e)=>handleSubmit(e)} className='form-main'> */}
+
+        <form onSubmit={(e)=> e.preventDefault()} className='form-main'>
 
             {fields && generateFields(fields, data, setData)}
-            <button type='submit'>{props.button}</button>
+            {/* <button type='submit'>{props.button}</button> */}
+            <button onClick={onSubmit}> {props.button}</button>
 
         </form>
+        </FormProvider>
     )
 }
 
--- frontend/src/components/common/input/Input.js
+++ frontend/src/components/common/input/Input.js
@@ -1,12 +1,32 @@
 import './Input.scss'
-
+import {useFormContext} from 'react-hook-form'
 const Input = ({label, name, type, placeholder, setData}) => {
+    const { 
+        register , 
+        formState: { errors }
+    } = useFormContext()
+
     return (
         <div className='input-main'>
             <label htmlFor={name}> {label} </label> <br></br>
-            <input type={type} name={name} id={name} placeholder={placeholder} onChange={setData}></input>
+            <input 
+            
+                type={type} 
+                name={name} 
+                id={name} 
+                placeholder={placeholder} 
+                onChange={setData}
+                {...register(label, {
+                    required: true,
+                    message: 'This field is required'
+                })}
+                
+                />
+
         </div>
     )
 }
+
+
 
 export default Input
