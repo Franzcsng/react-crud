1096b4bebbb5ac048a6215c76d6be317e8e7d023
Francis Causing <franzcsng23@gmail.com>
2025-10-18 23:43:15 +0800

    Create: create validation helpers findInputError and isFormValid

--- frontend/src/helpers/utils/utils.js
+++ frontend/src/helpers/utils/utils.js
@@ -1,3 +1,14 @@
-export const validateInput = () => {
-    
+export const findInputError = (errors, name) => {
+    const filtered = Object.keys(errors)
+    .filter(key => key.includes(name))
+    .reduce((cur, key) => {
+        return Object.assign(cur, { error: errors[key]})
+    }, {})
+
+    return filtered
+}
+
+export const isFormValid = err => {
+    if (Object.keys(err).length > 0) {return true}
+    else {return false}
 }
