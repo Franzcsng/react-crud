5748cb0b6802b10a69c2d1d1a0061baddfa79036
Francis Causing <franzcsng23@gmail.com>
2025-10-18 23:42:18 +0800

    Add: call validation from utils for validation error and extract input validation into prop

--- frontend/src/components/common/input/Input.js
+++ frontend/src/components/common/input/Input.js
@@ -1,26 +1,32 @@
 import './Input.scss'
 import {useFormContext} from 'react-hook-form'
-const Input = ({label, name, type, placeholder, setData}) => {
+import { findInputError, isFormValid } from '../../../helpers/utils/utils'
+
+const Input = ({label, name, type, placeholder, setData, validation, }) => {
     const { 
         register , 
         formState: { errors }
     } = useFormContext()
 
+    const inputError =  findInputError(errors, label)
+    const isInvalid = isFormValid(inputError)
+
+    console.log(Object.keys(inputError).length)
     return (
         <div className='input-main'>
+            {isInvalid && (
+                <p style={{color: 'red'}}>{inputError.error.message}</p>
+            )}
             <label htmlFor={name}> {label} </label> <br></br>
             <input 
-            
                 type={type} 
                 name={name} 
                 id={name} 
                 placeholder={placeholder} 
                 onChange={setData}
-                {...register(label, {
-                    required: true,
-                    message: 'This field is required'
-                })}
-                
+
+                {...register(label, validation)}
+             
                 />
 
         </div>
